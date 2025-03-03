49d2b272d9f5f5a888822a212a42986485444fa9
Edward Ruzek <edadiddles@hotmail.com>
2025-03-03 09:05:41 -0500

    adding repl with break upon winning

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -6,21 +6,26 @@ fn main() {
     println!("Welcome to the game, Guess the Number!");
     let secret_number = rand::rng().random_range(1..=100);
 
-    println!("Please input your guess. (1-100)");
+    loop {
+        println!("Please input your guess. (1-100)");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    let guess: i32 = guess.trim().parse().expect("Please type a number!");
+        let guess: i32 = guess.trim().parse().expect("Please type a number!");
 
-    println!("You guessed: {guess}");
+        println!("You guessed: {guess}");
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You Win!"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => break,
+        }
     }
+
+    println!("You win!");
+
 }
