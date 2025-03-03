0a53f9f95e13b2d497c4cb4c19b7985815254e75
Edward Ruzek <edadiddles@hotmail.com>
2025-03-03 08:44:40 -0500

    adding rng from 1-100 inclusive

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,9 +1,11 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Welcome to the game, Guess the Number!");
+    let secret_number = rand::rng().random_range(1..=100);
 
-    println!("Please input your guess.");
+    println!("Please input your guess. (1-100)");
 
     let mut guess = String::new();
 
@@ -11,5 +13,6 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
 
+    println!("The secret number is: {secret_number}");
     println!("You guessed: {}", guess);
 }
