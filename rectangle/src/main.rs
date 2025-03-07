733a76f844edf4d8f3fbc37cc37aa442a38a1acc
Edward Ruzek <edadiddles@hotmail.com>
2025-03-07 07:36:24 -0500

    adding debug printed output

--- rectangle/src/main.rs
+++ rectangle/src/main.rs
@@ -1,20 +1,23 @@
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
 }
 
 fn main() {
-    let rec1: Rectangle = Rectangle {
+    let rect1: Rectangle = Rectangle {
         width: 30,
         height: 80
     };
 
     println!(
         "The area of the rectangle is {} square pixels.",
-        area(&rec1)
+        area(&rect1)
     );
+
+    println!("rect1 is {rect1:?}")
 }
 
-fn area (rec: &Rectangle) -> u32 {
-    rec.width * rec.height
+fn area (rect: &Rectangle) -> u32 {
+    rect.width * rect.height
 }
