01531f070ec395a46cdd62e7de4ce0ae543499c3
Edward Ruzek <edadiddles@hotmail.com>
2025-03-07 08:02:13 -0500

    playing around with println and dbg macros

--- rectangle/src/main.rs
+++ rectangle/src/main.rs
@@ -5,8 +5,9 @@ struct Rectangle {
 }
 
 fn main() {
+    let scale = 2;
     let rect1: Rectangle = Rectangle {
-        width: 30,
+        width: dbg!(30*scale),
         height: 80
     };
 
@@ -15,7 +16,9 @@ fn main() {
         area(&rect1)
     );
 
-    println!("rect1 is {rect1:?}")
+    dbg!(&rect1);
+    println!("rect1 is {rect1:?}");
+    println!("rect1 is {rect1:#?}");
 }
 
 fn area (rect: &Rectangle) -> u32 {
