4c502709128afb5bd1e24ad1ed91a58d0179e32f
Edward Ruzek <edadiddles@hotmail.com>
2025-03-07 07:23:28 -0500

    simple refactor into simple tuple

--- rectangle/src/main.rs
+++ rectangle/src/main.rs
@@ -1,13 +1,12 @@
 fn main() {
-    let width1 = 30;
-    let height1 = 80;
+    let rec1 = (30,80);
 
     println!(
         "The area of the rectangle is {} square pixels.",
-        area(width1, height1)
+        area(rec1)
     );
 }
 
-fn area (width: u32, height: u32) -> u32 {
-    width * height
+fn area (rec: (u32,u32)) -> u32 {
+    rec.0 * rec.1
 }
