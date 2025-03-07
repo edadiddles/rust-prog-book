7585a5c9f95d83a68fbed7ab686256c5fd1c627f
Edward Ruzek <edadiddles@hotmail.com>
2025-03-07 08:18:26 -0500

    adding new method that references another instance of Rectangle

--- rectangle/src/main.rs
+++ rectangle/src/main.rs
@@ -7,12 +7,24 @@ impl Rectangle {
     fn area (&self) -> u32 {
         self.width * self.height
     }
+
+    fn can_hold(&self, rect: &Rectangle) -> bool {
+        self.width > rect.width && self.height > rect.height
+    }
 }
 fn main() {
     let scale = 2;
     let rect1: Rectangle = Rectangle {
         width: dbg!(30*scale),
         height: 80
+    };
+    let rect2: Rectangle = Rectangle {
+        width: 50,
+        height: 90
+    };
+    let rect3: Rectangle = Rectangle {
+        width: 10,
+        height: 30
     };
 
     println!(
@@ -23,5 +35,8 @@ fn main() {
     dbg!(&rect1);
     println!("rect1 is {rect1:?}");
     println!("rect1 is {rect1:#?}");
+
+    println!("rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
 
