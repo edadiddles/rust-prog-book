d651864dedbb2d92d7ab3031b6e44b2398abb89e
Edward Ruzek <edadiddles@hotmail.com>
2025-03-09 08:27:35 -0400

    playing around with structs and enums privacy

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -21,12 +21,36 @@ mod back_of_house {
     }
 
     fn cook_order() {}
+
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
 }
 
 fn deliver_order() {}
 
 pub fn eat_at_restaurant() {
-    crate::front_of_house::hosting::add_to_waitlist();
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
 
-    front_of_house::hosting::add_to_waitlist();
+    //meal.seasonal_fruit = String::from("blueberries");
+    
+    let _order1 = back_of_house::Appetizer::Soup;
+    let _order2 = back_of_house::Appetizer::Salad;
 }
