f49ea9c0d29b93fe2791628cbb6e8bdbc07d809b
Edward Ruzek <edadiddles@hotmail.com>
2025-03-17 07:57:42 -0400

    small refactor to parse input vals into structure

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -7,12 +7,22 @@ fn main() {
     if args.len() < 3 {
         panic!("Expected query and file_path inputs");
     }
-    let query = &args[1];
-    let file_path = &args[2];
+    let config = parse_config(&args);
+    println!("Searching for {}",config.query);
+    println!("In file {}",config.file_path);
 
-    println!("Searching for {query}");
-    println!("In file {file_path}");
-
-    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+    let contents = fs::read_to_string(config.file_path).expect("Should have been able to read the file");
     println!("With text:\n{contents}");
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let query = &args[1].clone();
+    let file_path = &args[2].clone();
+    
+    Config{ query: query.to_string(), file_path: file_path.to_string() }
 }
